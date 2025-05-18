334f139755a549e7dd53f37318ac404d2ef104fd
Vansh Garg <147059527+VANSHG2005@users.noreply.github.com>
2025-05-18 23:17:30 +0530

    api key removed

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const fileUploadWrapper = promptForm.querySelector(".file-upload-wrapper");
 const themeToggleBtn = document.querySelector("#theme-toggle-btn");
 
 // API Setup
-const API_KEY = API_KEY;
+const API_KEY = api_key;
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 let controller, typingInterval;
 const chatHistory = [];
